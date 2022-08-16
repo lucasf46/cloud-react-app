6828f2067aace42defba9e4e1ec1db5d1373dd03
LucasF46 <lhdflemming@gmail.com>
2022-08-15 23:29:36 -0500

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    CloudUploadIcon,
+    DatabaseIcon,
+    PaperAirplaneIcon,
+    ServerIcon,
+
+} from '@heroicons/react/solidc'
+
+
+function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
